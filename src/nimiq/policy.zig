d2bcf68149b18388c008e3fa0f001879d56a3cef
redmaner <jakevdputten@protonmail.com>
2024-09-20 12:25:18 +0200

    nimiq: policy: add docs and test

--- src/nimiq/policy.zig
+++ src/nimiq/policy.zig
@@ -14,36 +14,44 @@ pub const collection_batches = policy.collection_batches;
 pub const collection_size = policy.collection_size;
 pub const network_id: u8 = policy.network_id;
 
+/// Returns the block number for the given batch number
 pub fn getBlockNumberForBatch(batch_number: u32) u32 {
     return batch_number * batch_size + genesis_number;
 }
 
+/// Returns the block number for the given epoch number
 pub fn getBlockNumberForEpoch(epoch_number: u32) u32 {
     return epoch_number * epoch_size + genesis_number;
 }
 
+/// Returns the batch number for the given block number
 pub fn getBatchFromBlockNumber(block_number: u32) u32 {
     return calculateSizeForBlock(block_number, batch_size);
 }
 
+/// Returns the collections number for the given block number
 pub fn getCollectionFromBlockNumber(block_number: u32) u32 {
     if (block_number < batch_size) return 0;
     return calculateSizeForBlock(block_number - batch_size, collection_size);
 }
 
+/// Returns the collection number for he given batch number
 pub fn getCollectionFromBatchNumber(batch_number: u32) u32 {
     return getCollectionFromBlockNumber(getBlockNumberForBatch(batch_number));
 }
 
+/// Returns the first batch number part of the given collection number
 pub fn getFirstBatchFromCollection(collection_number: u32) u32 {
     const block_number = ((collection_number - 1) * collection_size) + batch_size + 1 + genesis_number;
     return getBatchFromBlockNumber(block_number);
 }
 
+/// Returns the epoch number for the given batch number
 pub fn getEpochFromBatchNumber(batch_number: u32) u32 {
     return calculateSizeForBlock(getBlockNumberForBatch(batch_number), epoch_size);
 }
 
+/// Returns the epoch number for the given block number
 pub fn getEpochFromBlockNumber(block_number: u32) u32 {
     return calculateSizeForBlock(block_number, epoch_size);
 }
@@ -53,6 +61,7 @@ fn calculateSizeForBlock(block_number: u32, size: comptime_int) u32 {
     return @intFromFloat(math.ceil(@as(f64, @floatFromInt(number)) / size));
 }
 
+/// Returns the block type for the given block number
 pub fn getBlockTypeByBlockNumber(block_number: u32) BlockType {
     const number = block_number - genesis_number;
 
@@ -65,6 +74,13 @@ pub fn getBlockTypeByBlockNumber(block_number: u32) BlockType {
     }
 
     return BlockType.Micro;
+}
+
+/// Returns whether one or macro blocks has passed since original block height, compared to
+/// current block height. Macro blocks provide finality on chain, so once a macro block has passed
+/// original height can be considered final / confirmed.
+pub fn macroBlockPassedSince(original_block: u32, current_block: u32) bool {
+    return getBatchFromBlockNumber(current_block) > getBatchFromBlockNumber(original_block);
 }
 
 test "policy unittest constants" {
@@ -91,4 +107,15 @@ test "get collection from block number" {
     try testing.expect(getCollectionFromBlockNumber(120) == 1);
     try testing.expect(getFirstBatchFromCollection(1) == 2);
     try testing.expect(getFirstBatchFromCollection(2) == 242);
+}
+
+test "block types by block number" {
+    try testing.expect(getBlockTypeByBlockNumber(60) == BlockType.Checkpoint);
+    try testing.expect(getBlockTypeByBlockNumber(43200) == BlockType.Election);
+    try testing.expect(getBlockTypeByBlockNumber(999) == BlockType.Micro);
+}
+
+test "macro block passed since" {
+    try testing.expect(!macroBlockPassedSince(70, 71));
+    try testing.expect(macroBlockPassedSince(110, 163));
 }
